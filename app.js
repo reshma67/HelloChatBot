0a411f98b8838f848638963b0660c444ab756c9e
Murali Krishnan <muralk@amazon.com>
2019-09-09 23:09:24 -0700

    initial version

--- app.js
+++ app.js
@@ -1,1 +1,28 @@
-console.log("hi");
+'use strict';
+
+let restify = require('restify')
+//Include the library botbuilder
+let builder = require('botbuilder')
+
+//Create the server
+let server = restify.createServer()
+
+//Run the server continuously
+server.listen(3978, function(){
+	console.log('The server is running on ', server.name, server.url)
+})
+
+// Create chat connector with the default id and password
+let connector = new builder.ChatConnector({
+	appId: process.env.MICROSOFT_APP_ID,
+    appPassword: process.env.MICROSOFT_APP_PASSWORD
+})
+
+//When the server posts to /api/messages, make the connector listen to it.
+server.post('/api/messages', connector.listen())
+
+let bot = new builder.UniversalBot(connector, (session)=>{
+	let name = session.message.user.name
+	let message = session.message.text
+	session.send(name + " said "  + message)
+})
